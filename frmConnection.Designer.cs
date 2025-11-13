7ded0f06b34281d9c1506b733bfb8d4f103b6130
Kamal Wakim <kamal05wakim@gmail.com>
2025-11-13 13:12:45 -0500

    Frm créé et fonctionalité presque fini

---
+++ frmConnection.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace ProjetII_B56 {
+    partial class frmConnection {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose( bool disposing ) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.lblMotPasse = new System.Windows.Forms.Label();
+            this.txtMotPasse = new System.Windows.Forms.TextBox();
+            this.btnConnexion = new System.Windows.Forms.Button();
+            this.bDB56Pr211DataSet = new ProjetII_B56.BDB56Pr211DataSet();
+            this.employesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.employesTableAdapter = new ProjetII_B56.BDB56Pr211DataSetTableAdapters.EmployesTableAdapter();
+            this.tableAdapterManager = new ProjetII_B56.BDB56Pr211DataSetTableAdapters.TableAdapterManager();
+            ((System.ComponentModel.ISupportInitialize)(this.bDB56Pr211DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employesBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.Location = new System.Drawing.Point(12, 16);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(148, 20);
+            this.lblUserName.TabIndex = 0;
+            this.lblUserName.Text = "Nom d\'utilisateur:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(166, 18);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(130, 20);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // lblMotPasse
+            // 
+            this.lblMotPasse.AutoSize = true;
+            this.lblMotPasse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMotPasse.Location = new System.Drawing.Point(12, 53);
+            this.lblMotPasse.Name = "lblMotPasse";
+            this.lblMotPasse.Size = new System.Drawing.Size(122, 20);
+            this.lblMotPasse.TabIndex = 2;
+            this.lblMotPasse.Text = "Mot de passe:";
+            // 
+            // txtMotPasse
+            // 
+            this.txtMotPasse.Location = new System.Drawing.Point(166, 55);
+            this.txtMotPasse.Name = "txtMotPasse";
+            this.txtMotPasse.Size = new System.Drawing.Size(130, 20);
+            this.txtMotPasse.TabIndex = 3;
+            // 
+            // btnConnexion
+            // 
+            this.btnConnexion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnexion.Location = new System.Drawing.Point(66, 97);
+            this.btnConnexion.Name = "btnConnexion";
+            this.btnConnexion.Size = new System.Drawing.Size(171, 22);
+            this.btnConnexion.TabIndex = 4;
+            this.btnConnexion.Text = "Connectez-Vous";
+            this.btnConnexion.UseVisualStyleBackColor = true;
+            this.btnConnexion.Click += new System.EventHandler(this.btnConnexion_Click);
+            // 
+            // bDB56Pr211DataSet
+            // 
+            this.bDB56Pr211DataSet.DataSetName = "BDB56Pr211DataSet";
+            this.bDB56Pr211DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // employesBindingSource
+            // 
+            this.employesBindingSource.DataMember = "Employes";
+            this.employesBindingSource.DataSource = this.bDB56Pr211DataSet;
+            // 
+            // employesTableAdapter
+            // 
+            this.employesTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.AbonnementsTableAdapter = null;
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.DependantsTableAdapter = null;
+            this.tableAdapterManager.DepensesTableAdapter = null;
+            this.tableAdapterManager.EmployesTableAdapter = this.employesTableAdapter;
+            this.tableAdapterManager.PartiesJoueesTableAdapter = null;
+            this.tableAdapterManager.PrixDepensesAbonnementsTableAdapter = null;
+            this.tableAdapterManager.ProvincesTableAdapter = null;
+            this.tableAdapterManager.ReabonnementsTableAdapter = null;
+            this.tableAdapterManager.ServicesTableAdapter = null;
+            this.tableAdapterManager.TerrainsTableAdapter = null;
+            this.tableAdapterManager.TypesAbonnementTableAdapter = null;
+            this.tableAdapterManager.TypesEmployeTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = ProjetII_B56.BDB56Pr211DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // frmConnection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 151);
+            this.Controls.Add(this.btnConnexion);
+            this.Controls.Add(this.txtMotPasse);
+            this.Controls.Add(this.lblMotPasse);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lblUserName);
+            this.Name = "frmConnection";
+            this.Text = "Connexion";
+            this.Load += new System.EventHandler(this.frmConnection_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bDB56Pr211DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employesBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label lblMotPasse;
+        private System.Windows.Forms.TextBox txtMotPasse;
+        private System.Windows.Forms.Button btnConnexion;
+        private BDB56Pr211DataSet bDB56Pr211DataSet;
+        private System.Windows.Forms.BindingSource employesBindingSource;
+        private BDB56Pr211DataSetTableAdapters.EmployesTableAdapter employesTableAdapter;
+        private BDB56Pr211DataSetTableAdapters.TableAdapterManager tableAdapterManager;
+    }
+}
+
